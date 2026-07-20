190e688b1983f502af0cea1159db087eb6fca8f4
Birat Tripathee <birattripathee.gandaki@gmail.com>
2026-07-20 19:06:20 +0545

    code updated
    fightingcontroller.cs code updated

--- Assets/Game/Scripts/FightingController/FightingController.cs
+++ Assets/Game/Scripts/FightingController/FightingController.cs
@@ -1,11 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FightingController : MonoBehaviour
 {
+    [Header("Movement")]
     public float movementSpeed = 1f;
     public float rotationSpeed = 10f;
+
     private CharacterController characterController;
     private Animator animator;
 
@@ -13,38 +13,54 @@ public class FightingController : MonoBehaviour
     {
         characterController = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
+
+        if (characterController == null)
+        {
+            Debug.LogError("CharacterController component is missing on " + gameObject.name);
+        }
+
+        if (animator == null)
+        {
+            Debug.LogError("Animator component is missing on " + gameObject.name);
+        }
+    }
+
+    void Update()
+    {
+        PerformMovement();
     }
 
     void PerformMovement()
     {
+        if (characterController == null || animator == null)
+            return;
+
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(-verticalInput, 0f, horizontalInput);
 
-        if (movement != Vector3.zero)
+        if (movement.magnitude > 0.01f)
         {
             Quaternion targetRotation = Quaternion.LookRotation(movement);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
 
-            if (horizontalInput > 0)
-            {
-                animator.SetBool("Walking", true);
-            }
-            else if (horizontalInput < 0)
-            {
-                animator.SetBool("Walking", true);
-            }
-            else if (verticalInput != 0)
-            {
-                animator.SetBool("Walking", true);
-            }
+            transform.rotation = Quaternion.Slerp(
+                transform.rotation,
+                targetRotation,
+                rotationSpeed * Time.deltaTime
+            );
+
+            animator.SetBool("Walking", true);
         }
         else
         {
             animator.SetBool("Walking", false);
         }
 
-        characterController.Move(movement * movementSpeed * Time.deltaTime);
+        characterController.Move(
+            movement.normalized *
+            movementSpeed *
+            Time.deltaTime
+        );
     }
 }
