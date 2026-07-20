5ffb4f7764a7f2187466b942e5c39e27326846ee
Birat Tripathee <birattripathee.gandaki@gmail.com>
2026-07-20 17:56:51 +0545

    fighting controller script
    fighting controller script added with csharp language with certain movement and other functions like idle, dodge, walk . buttons sets

--- Assets/Game/Scripts/FightingController/FightingController.cs
+++ Assets/Game/Scripts/FightingController/FightingController.cs
@@ -1,16 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class FightingController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float movementSpeed = 1f;
+    public float rotationSpeed = 10f;
+    private CharacterController characterController;
+    private Animator animator;
+
+    void Awake()
     {
-        
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void PerformMovement()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(-verticalInput, 0f, horizontalInput);
+
+        if (movement != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+            if (horizontalInput > 0)
+            {
+                animator.SetBool("Walking", true);
+            }
+            else if (horizontalInput < 0)
+            {
+                animator.SetBool("Walking", true);
+            }
+            else if (verticalInput != 0)
+            {
+                animator.SetBool("Walking", true);
+            }
+        }
+        else
+        {
+            animator.SetBool("Walking", false);
+        }
+
+        characterController.Move(movement * movementSpeed * Time.deltaTime);
     }
 }
