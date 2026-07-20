3f3d0e94b02bfb0730cd135503d9c425493dfedd
Birat Tripathee <birattripathee.gandaki@gmail.com>
2026-07-20 22:19:56 +0545

    scripted updated
    with more features

--- Assets/Game/Scripts/FightingController/FightingController.cs
+++ Assets/Game/Scripts/FightingController/FightingController.cs
@@ -1,66 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;   
 using UnityEngine;
 
 public class FightingController : MonoBehaviour
 {
-    [Header("Movement")]
+    [Header("Player Movement")]
     public float movementSpeed = 1f;
     public float rotationSpeed = 10f;
-
     private CharacterController characterController;
     private Animator animator;
+
+    [Header("Player Fight")]
+    public float attackCooldown = 0.5f;
+    public int attackDamages = 5;
+    public string[] attackAnimations = { "Attack1Animation", "Attack2Animation", "Attack3Animation", "Attack4Animation" };
+    private float lastAttackTime;
 
     void Awake()
     {
         characterController = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
-
-        if (characterController == null)
-        {
-            Debug.LogError("CharacterController component is missing on " + gameObject.name);
-        }
-
-        if (animator == null)
-        {
-            Debug.LogError("Animator component is missing on " + gameObject.name);
-        }
     }
 
     void Update()
     {
         PerformMovement();
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            PerformAttack(0);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            PerformAttack(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            PerformAttack(2);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            PerformAttack(3);
+        }
     }
 
     void PerformMovement()
     {
-        if (characterController == null || animator == null)
-            return;
-
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(-verticalInput, 0f, horizontalInput);
 
-        if (movement.magnitude > 0.01f)
+        if (movement != Vector3.zero)
         {
             Quaternion targetRotation = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
 
-            transform.rotation = Quaternion.Slerp(
-                transform.rotation,
-                targetRotation,
-                rotationSpeed * Time.deltaTime
-            );
-
-            animator.SetBool("Walking", true);
+            if (horizontalInput > 0)
+            {
+                animator.SetBool("Walking", true);
+            }
+            else if (horizontalInput < 0)
+            {
+                animator.SetBool("Walking", true);
+            }
+            else if (verticalInput != 0)
+            {
+                animator.SetBool("Walking", true);
+            }
         }
         else
         {
             animator.SetBool("Walking", false);
         }
 
-        characterController.Move(
-            movement.normalized *
-            movementSpeed *
-            Time.deltaTime
-        );
+        characterController.Move(movement * movementSpeed * Time.deltaTime);
+    }
+
+    void PerformAttack(int attackIndex)
+    {
+        if (Time.time - lastAttackTime > attackCooldown)
+        {
+            animator.Play(attackAnimations[attackIndex]);
+
+            int damage = attackDamages;
+            Debug.Log("Performed attack " + (attackIndex + 1) + " dealing " + damage + "damage");
+
+            lastAttackTime = Time.time;
+
+            // Loop through each opponent
+        }
+        else
+        {
+            // If the player tries to perform an attack too quickly, inform them
+            Debug.Log("Cannot perform attack yet. Cooldown time remaining.");
+        }
     }
 }
