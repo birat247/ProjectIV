00e9212c0bcdd7e6a69925d1ef6e77d2515b97c5
Birat Tripathee <birattripathee.gandaki@gmail.com>
2026-07-21 17:01:25 +0545

    written OpponentAI
    written OpponentAI  in c# language

--- Assets/Game/Scripts/Opponent/OpponentAI.cs
+++ Assets/Game/Scripts/Opponent/OpponentAI.cs
@@ -1,16 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class OpponentAI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Oppponent Movement")]
+    public float movementSpeed = 1f;
+    public float rotationSpeed = 10f;
+    private CharacterController characterController;
+    private Animator animator;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Opponent Fight")]
+    public float attackCooldown = 0.5f;
+    public int attackDamages = 5;
+    public string[] attackAnimations = { "Attack1Animation", "Attack2Animation", "Attack3Animation", "Attack4Animation" };
+    public float dodgeDistance = 2f;
+    public int attackCount = 0;
+    public int randomNumber;
+    public float attackRadius = 2f;
+    public FightingController[] fightingController;
+    public Transform[] players;
+    public bool isTakingDamage;
+    private float lastAttackTime;
+
+    [Header("Effects and Sound")]
+    public ParticleSystem attack1Effect;
+    public ParticleSystem attack2Effect;
+    public ParticleSystem attack3Effect;
+    public ParticleSystem attack4Effect;
 }
