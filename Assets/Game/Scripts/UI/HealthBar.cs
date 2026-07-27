e9a92123c7442ffa6592477fc1571821bf758ed7
Birat Tripathee <birattripathee.gandaki@gmail.com>
2026-07-27 10:49:22 +0545

    health script implemented

--- Assets/Game/Scripts/UI/HealthBar.cs
+++ Assets/Game/Scripts/UI/HealthBar.cs
@@ -1,16 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Slider healthBarSlider;
+
+    public void GiveFullHealth(float health)
     {
-        
+        healthBarSlider.maxValue = health;
+        healthBarSlider.value = health;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetHealth(float health)
     {
-        
+        healthBarSlider.value = health;
     }
 }
