f1e07b77e99edcb13111aa737fe697327ddd7bce
Birat Tripathee <birattripathee.gandaki@gmail.com>
2026-08-01 22:20:02 +0545

    MainMenue.cs script written sucessfully
    MainMenue.cs script written sucessfully with functional working.

--- Assets/Game/Scripts/UI/MainMenu.cs
+++ Assets/Game/Scripts/UI/MainMenu.cs
@@ -1,16 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject mainMenu;
+    public GameObject selectCharacterAndStageMenu;
+    public GameObject optionMenu;
+    public GameObject controlsMenu;
+
     void Start()
     {
-        
+        mainMenu.SetActive(true);
+        selectCharacterAndStageMenu.SetActive(false);
+        optionMenu.SetActive(false);
+        controlsMenu.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayButtonClicked()
     {
-        
+        mainMenu.SetActive(false);
+        selectCharacterAndStageMenu.SetActive(true);
+    }
+
+    public void OptionsButtonClicked()
+    {
+        mainMenu.SetActive(false);
+        optionMenu.SetActive(true);
+    }
+
+    public void ControlsButtonClicked()
+    {
+        optionMenu.SetActive(false);
+        controlsMenu.SetActive(true);
+    }
+
+    public void ExitButtonClicked()
+    {
+        Application.Quit();
+    }
+
+    public void BackButtonClicked()
+    {
+        mainMenu.SetActive(true);
+        selectCharacterAndStageMenu.SetActive(false);
+        optionMenu.SetActive(false);
+        controlsMenu.SetActive(false);
+    }
+
+    public void SelectCharacterClicked(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void SelectStageClicked(string sceneName)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneName);
     }
 }
